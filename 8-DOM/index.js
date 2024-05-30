9c90138d92e2c2e472beeb3bc86e7186f4a91965
LuisMirandaJr <lmirandajr@hotmail.com>
2024-05-29 22:22:42 -0300

    Meu curso

---
+++ 8-DOM/index.js
@@ -0,0 +1,26 @@
+//alert("Olá")
+
+var titulo = document.querySelector("h1");
+titulo.style.color = "red";
+console.log (titulo);
+
+
+
+var botao = document.querySelector("button");
+//botao.style.background = "aqua";
+console.log (botao);
+
+
+var botao = document.querySelector(".my-button");
+//botao.style.backgroundColor = "green";
+
+
+console.log(botao.classList);
+
+botao.classList.add("btn");
+console.log(botao.classList);
+
+
+
+
+
---
+++ desafio_14/index.js
@@ -0,0 +1,11 @@
+var corpo = document.querySelector("body");
+corpo.style.backgroundColor = "green";
+console.log(corpo);
+
+var fonte = document.querySelector("h1");
+fonte.style.fontStyle = "italic";
+console.log(fonte);
+
+var cor = document.querySelectorAll("p");
+cor.style.color = "red";
+console.log(cor);
